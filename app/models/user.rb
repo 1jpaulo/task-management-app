76f354153cdc0f14912a7db1e1158aec60ef089d
Joao Paulo Alvarenga <joao1310al@gmail.com>
2024-09-01 00:37:28 -0300

    Add turbo confirmation popup for task deletion. Implement validations and tests for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :tasks
+  # TODO: TEST all of it below.
+  has_many :tasks, dependent: :destroy
+
+  # No registration page in this application, but this validations would apply for console operations as well.
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, on: :create }
+  validates :username, uniqueness: true, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new(username: "hey", email: "there@example.com", password: BCrypt::Password.create("secret"))
+  end
+
+  test "should create user with valid attributes" do
+    assert @user.save
+  end
+
+  test "should not create user with invalid email" do
+    @user.email = nil
+    @user.save
+
+    assert_equal 2, @user.errors.count
+    assert_equal 2, (["Email is invalid", "Email can't be blank"] & @user.errors.full_messages).count
+  end
+
+  test "should not create user with invalid username" do
+    @user.username = nil
+    @user.save
+
+    assert_equal 1, @user.errors.count
+    assert_equal "Username can't be blank", @user.errors.first.full_message
+  end
+
+  test "should not create user with duplicated email" do
+    @user.email = users(:one).email
+    @user.save
+
+    assert_equal 1, @user.errors.count
+    assert_equal "Email has already been taken", @user.errors.first.full_message
+  end
+
+  test "should not create user with duplicated username" do
+    @user.username = users(:one).username
+    @user.save
+
+    assert_equal 1, @user.errors.count
+    assert_equal "Username has already been taken", @user.errors.first.full_message
+  end
 end
