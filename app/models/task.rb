7eee8e2b4e4df075360e06eae73be0a87ad5ed6b
Joao Paulo Alvarenga <joao1310al@gmail.com>
2024-09-01 01:39:29 -0300

    Add validations and tests for task model. Enforce a list contained value for task's priority attribute and update fixtures accordingly.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  PRIORITY_LIST = %w[ low medium high urgent].freeze
+
+  validates :title, presence: true
+  validates :priority, presence: true, inclusion: { in: PRIORITY_LIST }
+  validates :due_date, presence: true
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @task = users(:one).tasks.new(title: "MyString",
+                                  description: "MyText",
+                                  priority: "high",
+                                  due_date: Time.now)
+  end
+
+  test "should create task with valid attributes" do
+    assert @task.save
+  end
+
+  test "should not create task without title" do
+    @task.title = nil
+
+    assert_raises(ActiveRecord::RecordInvalid, match: "Title can't be blank") do
+      @task.save!
+    end
+  end
+
+  test "should not create task without priority" do
+    @task.priority = nil
+
+    assert_raises(ActiveRecord::RecordInvalid, match: "Priority can't be blank") do
+      @task.save!
+    end
+  end
+
+  test "should not create task without due date" do
+    @task.due_date = nil
+
+    assert_raises(ActiveRecord::RecordInvalid, match: "Due date can't be blank") do
+      @task.save!
+    end
+  end
+
+  test "should not create task with priority value outside priority list" do
+    @task.priority = "something"
+
+    assert_raises(ActiveRecord::RecordInvalid, match: "Priority is not included in the list") do
+      @task.save!
+    end
+  end
 end
