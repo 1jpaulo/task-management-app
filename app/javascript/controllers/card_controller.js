e8b640ac4614d3a581804710db92acb7194cef63
Joao Paulo Alvarenga <joao1310al@gmail.com>
2024-12-03 20:23:03 -0300

    Change create button disable logic to use event listeners instead of timeout that can be unreliable.

--- app/javascript/controllers/card_controller.js
+++ app/javascript/controllers/card_controller.js
@@ -4,6 +4,21 @@ export default class extends Controller {
   static targets = ['newCard', 'createButton']
   static values = {
     isExpanded: Boolean,
+    isNew: Boolean,
+  }
+
+  /**
+   * Dispatches a card added event when state is new is true,
+   * which will disable the create button until the card is added to the workspace
+   */
+  connect() {
+    if (this.isNewValue) {
+      this.element.dispatchEvent(new CustomEvent("card:added", { bubbles: true }))
+      // remove state of new card since it was already added
+      this.isNewValue = false
+    } else {
+      this.element.addEventListener("card:added", this.disableButton.bind(this))
+    }
   }
 
   initialize() {
@@ -17,22 +32,14 @@ export default class extends Controller {
     this.element.style.transition = 'all .4s ease-in'
   }
 
-  // TODO: change name
-  transition() {
+  toggleCreateSlide() {
     this.newCardTarget.classList.toggle('max-h-[18rem]')
     this.newCardTarget.style.transition = 'all .4s ease-in'
-
-    // a delay is needed to make turbo-stream request be made
-    // TODO: remove setTimeout and try to use a response after turbo stream append, like we would have with a 
-    // `fetch(url).then(applyHtml)` approach, so we can disable the create button asynchronously
-    // or try to use a js.erb if this is evaluated after the turbo created
-    setTimeout(() => this.disableButton(), 100)
   }
 
   disableButton() {
     // TODO: enableButton again when losing focus of the card
     this.createButtonTarget.disabled = true
     this.createButtonTarget.classList.add('cursor-not-allowed', 'opacity-50')
-    
   }
 }
